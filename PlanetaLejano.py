fd7d52e276407b604c7b4295f94ed2174a86cf8a
Cesarjabmx <66344451+Cesarjabmx@users.noreply.github.com>
2022-10-04 16:38:35 -0500

    Add coments

--- PlanetaLejano.py
+++ PlanetaLejano.py
@@ -1,1 +1,8 @@
-print ("Prueba de crear el repo para después subir el programa hecho aquí")
+"""
+En un planeta muy extraño, los minutos son de 50 segundos, las horas de 70 minutos y los días de 12 horas. 
+Ya que planeas ir de vacaciones a ese planeta, te gustaría tener un reloj que dado un número de segundos , 
+te despliegue el número de días, horas, minutos y segundos de ese planeta. 
+Por ejemplo, para  han transcurrido 0 días, 1 hora, 1 minuto y 3 segundos.
+Escriba un programa para que tu reloj funcione correctamente.
+"""
+
